574549cc191478306581f8951016f5a3ef150655
Chandan Kumar Dubey <dubeychandan996@gmail.com>
2023-07-04 10:48:37 +0530

    Create email-verifier.php

---
+++ email-verifier.php
@@ -0,0 +1,21 @@
+<?php
+$email='info@proweblook.com';// number with country code
+$api_key='your apikey';  // you will found in proweblook dashboard in top nav bar
+$url='https://proweblook.com/api/v1/checkemail?email='.$email.'&api_key='.$api_key;
+
+$curl = curl_init();
+
+curl_setopt_array($curl, array(
+  CURLOPT_URL => $url,
+  CURLOPT_ENCODING => '',
+  CURLOPT_MAXREDIRS => 10,
+  CURLOPT_TIMEOUT => 0,
+  CURLOPT_FOLLOWLOCATION => true,
+  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+  CURLOPT_CUSTOMREQUEST => 'GET',
+));
+
+$response = curl_exec($curl);
+
+curl_close($curl);
+echo $response;
