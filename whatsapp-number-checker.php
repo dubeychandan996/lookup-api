f124b7d5ff151d6ba3f78ea273fe02e217a73f10
Chandan Kumar Dubey <dubeychandan996@gmail.com>
2023-07-04 10:40:17 +0530

    Update whatsapp-number-checker.php

--- whatsapp-number-checker.php
+++ whatsapp-number-checker.php
@@ -1,1 +1,24 @@
+<?php
+$num='919038655955';// number with country code
+$api_key='your apikey';  // you will found in proweblook dashboard in top nav bar
+$url='https://proweblook.com/api/v1/checkwanumber?number='.$num.'&api_key='.$api_key;
+
+$curl = curl_init();
+
+curl_setopt_array($curl, array(
+  CURLOPT_URL => $url,
+  CURLOPT_ENCODING => '',
+  CURLOPT_MAXREDIRS => 10,
+  CURLOPT_TIMEOUT => 0,
+  CURLOPT_FOLLOWLOCATION => true,
+  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+  CURLOPT_CUSTOMREQUEST => 'GET',
+));
+
+$response = curl_exec($curl);
+
+curl_close($curl);
+echo $response;
+
+
 
